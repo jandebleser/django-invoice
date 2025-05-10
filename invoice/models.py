0c9df03226cdebfcc6cb113cbf1c2f855143136e
Jan De Bleser <jandebleser@gmail.com>
2025-05-10 11:27:47 +0200

    Longer description field.

--- invoice/models.py
+++ invoice/models.py
@@ -130,7 +130,7 @@ class Invoice(TimeStampedModel):
 
 class InvoiceItem(models.Model):
     invoice = models.ForeignKey(Invoice, related_name='items', unique=False, on_delete=CASCADE)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
     unit_price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.DecimalField(max_digits=8, decimal_places=2, default=1)
 
