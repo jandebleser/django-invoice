8ebe8bb3fc13af2d54512680e956157b6f63a9a6
Jan De Bleser <jandebleser@gmail.com>
2026-01-20 14:55:34 +0100

    Adding pdf view for credit notes

--- invoice/views.py
+++ invoice/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
-from invoice.models import Invoice
+from invoice.models import Invoice, CreditNote
 from invoice.pdf import draw_pdf
 from invoice.utils import pdf_response
 
@@ -9,6 +9,9 @@ def pdf_view(request, pk):
     invoice = get_object_or_404(Invoice, pk=pk)
     return pdf_response(draw_pdf, invoice.file_name(), invoice)
 
+def credit_note_pdf_view(request, pk):
+    credit_note = get_object_or_404(CreditNote, pk=pk)
+    return pdf_response(draw_pdf, credit_note.file_name(), credit_note)
 
 @login_required
 def pdf_user_view(request, invoice_id):
